33350aabc02d13f1964d9326142eb1b38720fb93
Andrew Bateman <gomezbateman@yahoo.com>
2018-12-08 20:25:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Express backend server app to handle file uploads.
+Use for testing front end app.
+
+
+ref. FreeCodeCamp Medium article: 'How to set up file upload with React and Node' by Krissanawat​ Kaewsanmuang
+
+link:
+https://medium.freecodecamp.org/how-to-create-file-upload-with-react-and-node-2aa3f9aab3f0
---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const cors = require('cors')
+
+const app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+)
+app.use(cookieParser())
+app.use(fileUpload())
+app.use('/public', express.static(__dirname + '/public'))
+
+app.post('/upload', (req, res, next) => {
+  let uploadFile = req.files.file
+  const fileName = req.files.file.name
+  uploadFile.mv(
+    `${__dirname}/public/files/${fileName}`,
+    function (err) {
+      if (err) {
+        return res.status(500).send(err)
+      }
+
+      res.json({
+        file: `public/${req.files.file.name}`,
+      })
+    },
+  )
+})
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
