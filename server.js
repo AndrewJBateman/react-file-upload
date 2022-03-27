c846ab468d42be917e6e8b92e6a22840bf63bac5
AndrewJBateman <gomezbateman@yahoo.com>
2022-03-27 16:51:46 +0200

    chore: dependencies updated, code linting

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
 app.use(fileUpload());
 
 // Upload endpoint
-app.post('/upload', (req, res) => {
-  if(req.files === null) {
-    return res.status(400).json({msg: 'No file was uploaded'});
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file was uploaded" });
   }
 
   const file = req.files.file;
 
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-    if(err) {
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
+    if (err) {
       console.error(err);
       return res.status(500).send(err);
     }
-  
+
     res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
   });
 });
 
-app.listen(5000, () => console.log('Server started...'));
+app.listen(5000, () => console.log("Server started..."));
